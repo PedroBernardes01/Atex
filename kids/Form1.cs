b5b2dffa84d81fde4396b105c925b30692d81ce5
PedroBernardes01 <112957580+PedroBernardes01@users.noreply.github.com>
2023-05-12 21:53:32 -0300

    .

---
+++ kids/Form1.cs
@@ -0,0 +1,34 @@
+﻿using Mysql_Object;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Mysql_Object;
+
+namespace kids
+{
+    public partial class Form1 : Form
+    {
+        private WorkBench Querry;
+        public Form1(string host, string username, string schema, string password)
+        {
+            Querry = new WorkBench(host, username, schema, password);
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ kids/Mysql_Obj.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Mysql_Object
+{
+    public class WorkBench
+    {
+        private MySqlConnection Database;
+        public WorkBench(string host, string username, string schema, string password) =>
+            Database = new MySqlConnection($"server={host};User Id={username};database={schema}; password={password}");
+        public bool ALTER(string command)
+        {
+            try
+            {
+                Database.Open();
+                MySqlCommand msqlcmd = new MySqlCommand(command, Database);
+                msqlcmd.ExecuteNonQuery();
+                Database.Close();
+                return true;
+            }
+            catch (MySql.Data.MySqlClient.MySqlException)
+            {
+                return false;
+            }
+        }
+        public List<string[]> GET(string command, int QntTableElem)
+        {
+            try
+            {
+                Database.Open();
+                MySqlCommand msqlcmd = new MySqlCommand(command, Database);
+                MySqlDataReader mdr = msqlcmd.ExecuteReader();
+                string[] aux;
+                List<string[]> ListAux = new List<string[]>();
+                while (mdr.Read())
+                {
+                    aux = new string[QntTableElem];
+                    for (int i = 0; i < aux.Length; i++)
+                        aux[i] = mdr.GetString(i);
+                    ListAux.Add(aux);
+                }
+                Database.Close();
+                return ListAux;
+            }
+            catch (MySql.Data.MySqlClient.MySqlException)
+            {
+                return new List<string[]>();
+            }
+        }
+    }
+}
